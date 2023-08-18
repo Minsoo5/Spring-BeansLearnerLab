542b78db9d59903a1de83f894a14d990a97c1c0e
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-18 17:34:27 -0400

    Configs built

--- src/main/java/com/example/demo/Config/ClassroomConfig.java
+++ src/main/java/com/example/demo/Config/ClassroomConfig.java
@@ -1,4 +1,45 @@
 package java.com.example.demo.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+
+import java.com.example.demo.*;
+
 public class ClassroomConfig {
+    @Bean
+    @DependsOn({"instructors", "students"})
+    public Classroom currentCohort() {
+
+        Instructors inst = new Instructors<>();
+        inst.add(new Instructor(1l, "Kris"));
+        inst.add(new Instructor(2l, "Dolio"));
+        inst.add(new Instructor(3l, "Razz"));
+
+        Students st = new Students();
+        st.add(new Student(1l, "Joe"));
+        st.add(new Student(2l, "Brent"));
+        st.add(new Student(3l, "Maggie"));
+
+        Classroom classroom = new Classroom<>(inst, st);
+        return classroom;
+    }
+
+    @Bean
+    @DependsOn({"instructors", "students"})
+    public Classroom previousCohort() {
+
+        Instructors inst = new Instructors<>();
+        inst.add(new Instructor(1l, "Kris"));
+        inst.add(new Instructor(2l, "Dolio"));
+        inst.add(new Instructor(3l, "Razz"));
+
+        Students st = new Students();
+        st.add(new Student(1l, "Adam"));
+        st.add(new Student(2l, "Jen"));
+        st.add(new Student(3l, "Nina"));
+
+        Classroom classroom = new Classroom<>(inst, st);
+        return classroom;
+    }
+
 }
