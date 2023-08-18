14f2bd10eda9ad9fb25465a19ea43702f9e54a9e
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-18 15:19:32 -0400

    STREAMING THESE PEOPLE

--- src/main/java/com/example/demo/People.java
+++ src/main/java/com/example/demo/People.java
@@ -1,11 +1,39 @@
 package java.com.example.demo;
 
+import java.util.Collection;
 import java.util.List;
 
 public abstract class People <PersonType extends Person> implements Iterable<Person> {
 
-    private
-    public People(List<PersonType> peopleList){
-
+    private List<PersonType> personList;
+    public People(List<PersonType> personList) {
+        this.personList = personList;
     }
+
+    public void add(PersonType person) {
+        this.personList.add(person);
+    }
+
+    public void remove(PersonType person) {
+        personList.remove(person);
+    }
+
+    public int size() {
+        return this.personList.size();
+    }
+
+    public void clear() {
+//        personList.stream().forEach(p -> this.personList.remove(p));
+        this.personList.clear();
+    }
+
+    public void addAll(Iterable <PersonType> peopleList) {
+        this.personList.addAll((Collection<? extends PersonType>) peopleList);
+    }
+
+    public PersonType findById(long id) {
+        return this.personList.stream().filter(p -> p.getId() == id).findAny().get();
+    }
+
+
 }
