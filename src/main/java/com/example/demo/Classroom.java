8549299521e25bec01ef46455f625dd2f172b00c
Minsoo Jo <134647176+Minsoo5@users.noreply.github.com>
2023-08-18 16:35:21 -0400

    Classroom done

--- src/main/java/com/example/demo/Classroom.java
+++ src/main/java/com/example/demo/Classroom.java
@@ -1,5 +1,16 @@
 package java.com.example.demo;
 
-public class Classroom {
+public class Classroom <PeopleType extends People> {
+    private Instructors instructorsList;
+    private Students studentsList;
+
+    public Classroom(Instructors instructorsList, Students studentsList) {
+        this.instructorsList = instructorsList;
+        this.studentsList = studentsList;
+    }
+
+    public void hostLecture(Teacher teacher, double numberOfHours) {
+        teacher.lecture(this.studentsList, numberOfHours);
+    }
 
 }
--- src/main/java/com/example/demo/People.java
+++ src/main/java/com/example/demo/People.java
@@ -1,12 +1,13 @@
 package java.com.example.demo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public abstract class People <PersonType extends Person> implements Iterable<Person> {
 
-    private List<PersonType> personList;
+    private List<PersonType> personList = new ArrayList<>();
     public People(List<PersonType> personList) {
         this.personList = personList;
     }
